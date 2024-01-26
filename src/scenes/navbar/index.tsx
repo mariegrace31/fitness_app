f4f2e8b6351a687a3053e385adfc984208e2a962
mariegrace31 <mariegracebmg@gmail.com>
2024-01-26 18:09:46 +0200

    Update the navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -11,9 +11,11 @@ const Navbar = (props: Props) => {
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
+
             {/* LEFT SIDE */}
             <img src={Logo} alt="logo" />
             {/* RIGHT SIDE */}
+
             <div className={`${flexBetween} w-full`}>
               <div className={`${flexBetween} gap-8 text-sm`}>
                 <p>Home</p>
