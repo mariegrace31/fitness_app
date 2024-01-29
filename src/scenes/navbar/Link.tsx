cc09b76a25330331af923e7c0dd47ebe44f02c2e
mariegrace31 <mariegracebmg@gmail.com>
2024-01-29 20:14:18 +0200

    Update the links

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,1 +1,17 @@
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
+type Props = {
+  page: string;
+}
+
+const Link = ({
+  page,
+}: Props) => {
+  return (
+    <AnchorLink>
+      {page}
+    </AnchorLink>
+  )
+}
+
+export default Link;
