51469f20e62489e3364c9bbe3fc51f7453ba475e
mariegrace31 <mariegracebmg@gmail.com>
2024-02-01 21:54:10 +0200

    Add selected page types

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -2,13 +2,24 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 type Props = {
   page: string;
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
 }
 
 const Link = ({
   page,
+  selectedPage,
+  setSelectedPage,
 }: Props) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "")
   return (
-    <AnchorLink>
+    <AnchorLink
+    className={`${selectedPage === lowerCasePage} ? "text-primary-500" : ""
+    transition duration-500 hover:text-primary-300
+    `}
+    href={`#${lowerCasePage}`}
+    onClick={() => setSelectedPage(lowerCasePage)}
+    >
       {page}
     </AnchorLink>
   )
