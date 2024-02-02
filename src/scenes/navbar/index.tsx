0ca314965e9f4494917df9d6c64be601a1a49f53
mariegrace31 <mariegracebmg@gmail.com>
2024-02-02 16:36:30 +0200

    Add the responsiveness for medium screens

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -10,7 +11,9 @@ type Props = {
 }
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
-  const flexBetween = "flex items-center justify-between"
+  const flexBetween = "flex items-center justify-between";
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -21,7 +24,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             <img src={Logo} alt="logo" />
             {/* RIGHT SIDE */}
 
-            <div className={`${flexBetween} w-full`}>
+            {isAboveMediumScreens ? (<div className={`${flexBetween} w-full`}>
               <div className={`${flexBetween} gap-8 text-sm`}>
                 <Link page="Home"
                 selectedPage={selectedPage}
@@ -42,9 +45,17 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
               </div>
               <div className={`${flexBetween} gap-8`}>
                 <p>Sign In</p>
-                <button>Become a Member</button>
+                <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
               </div>
             </div>
+            ) : (
+              <button
+              className="rounded-full bg-secondary-500 p-2"
+              onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
         </div>
       </div>
