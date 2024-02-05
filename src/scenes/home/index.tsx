2e2fd4c98edad8fb27c45998cf5a49686910a90c
mariegrace31 <mariegracebmg@gmail.com>
2024-02-05 18:07:16 +0200

    Add the home page

---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,20 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { SelectedPage } from "@/shared/types";
+import ActionButton from "@/shared/ActionButton";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+}
+
+const Home = ({ setSelectedPage }: Props) => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home;
