ff322e8135ede2b7f99939c285e3be088a6a5fe0
mariegrace31 <mariegracebmg@gmail.com>
2024-02-05 18:35:57 +0200

    Add content and images to home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -6,14 +6,60 @@ import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 }
 
 const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
   return (
-    <div>Home</div>
+    <section
+    id="home"
+    className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
+    >
+      {/* IMAGE AND MAIN HEADER */}
+      <div>
+        {/* MAIN HEADER */}
+        <div>
+          {/* HEADINGS */}
+          <div>
+            <div>
+              <div>
+                <img alt="home-page-text" src={HomePageText}/>
+              </div>
+            </div>
+
+            <p>
+              Unrivaled Gym. Unparalleled Training Fitness Classes.The ultimate companion on your journey to a healthier you.
+              This dynamic Gym merges cutting-edge technology with personalized guidance, offering tailored workouts,
+              nutrition insights, and a vibrant community of fitness enthusiasts. It's more than just fitness; it's a lifestyle transformation.
+              Take control, embrace the challenge, and shape your body into the best version of yourself.
+            </p>
+          </div>
+          {/* ACTIONS */}
+          <div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+            className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+            onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+            href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
+        </div>
+        {/* IMAGE */}
+        <div>
+          <img alt="home-page-graphic" src={HomePageGraphic} />
+        </div>
+      </div>
+      {/* SPONSORS */}
+    </section>
   )
 }
 
