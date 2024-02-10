ab362d0fb484591a30f7f47ab8fcce9e5a84baba
mariegrace31 <mariegracebmg@gmail.com>
2024-02-10 21:34:25 +0200

    Add motion and transitions on the home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -22,12 +22,23 @@ const Home = ({ setSelectedPage }: Props) => {
     className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
     >
       {/* IMAGE AND MAIN HEADER */}
-      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+      <motion.div
+      className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+      onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
         {/* MAIN HEADER */}
         <div className="z-10 mt-32 md:basis-3/5">
           {/* HEADINGS */}
-          <motion.div 
+          <motion.div
+          initial="hidden"
+          whileInView="visible"
           className="md:-mt-20"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0},
+          }}
           >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
@@ -43,7 +54,17 @@ const Home = ({ setSelectedPage }: Props) => {
             </p>
           </motion.div>
           {/* ACTIONS */}
-          <div className="mt-8 flex items-center gap-8">
+          <motion.div
+          className="mt-8 flex items-center gap-8"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0},
+          }}
+          >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -54,14 +75,14 @@ const Home = ({ setSelectedPage }: Props) => {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
         {/* IMAGE */}
         <div className="flex basis-3/5 justify-center md:z-10
         md:ml-40 md:mt-16 md:justify-items-end">
           <img alt="home-page-graphic" src={HomePageGraphic} />
         </div>
-      </div>
+      </motion.div>
       {/* SPONSORS */}
       {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
